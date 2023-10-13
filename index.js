0307d8d7b8e9ab1aa6d9802b81c0a9c651a04ed8
tukang koding <147577468+Fadilahch@users.noreply.github.com>
2023-10-13 17:17:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert('wellcome')
+document.addEventListener("DOMContentLoaded", function () {
+    const getFriendsButton = document.getElementById("getFriendsButton");
+    const friendList = document.getElementById("friendList");
+
+    getFriendsButton.addEventListener("click", function () {
+        const accessToken = 'EAAU9UnVpvnkBO8HRQTLz2SFZBMtHaFQSWifvXKAap6ZCXpEMIW0402xe3agjZCZCxKIZBPm7FAsZAfQocZCBjPmEMbtzS7QWAat8np3mtkqIgSk3e3k7tonTYWu4locqrYNMxp4N0dB3Bozu6SRQpfRhzQZB2PhwNHRD4JJ2nei6ZAZAhZAs3yiBimc5jpLRDqwOtrSGpEKCXXclSwKekaeu9ztJtvDZCvLw6ZBgqw7K824pdRyI8UFu8vhNakAjdT01UhAZDZD'; // Gantilah dengan token akses Anda.
+        const userId = 'me'; // Ini akan mengambil teman dari profil Anda sendiri.
+
+        fetch(`https://graph.facebook.com/v10.0/${userId}/friends?access_token=${accessToken}`)
+            .then(response => response.json())
+            .then(data => {
+                const friends = data.data;
+                const friendsListHTML = friends.map(friend => `<li>${friend.name}</li>`).join('');
+                friendList.innerHTML = friendsListHTML;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    });
+});
+
